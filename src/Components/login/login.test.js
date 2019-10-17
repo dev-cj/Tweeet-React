79380fc0ab6086456459c4857658d7848c6a8b70
CJ <devcj.in@gmail.com>
2019-10-17 13:29:13 +0530

    added redux mock store package

--- src/Components/login/login.test.js
+++ src/Components/login/login.test.js
@@ -1,7 +1,30 @@
 import React from 'react'
 import { shallow } from 'enzyme';
 import LoginForm from './login'
+import "../../setupTest"
 
-it('should render without errors', () => {
-    expect(1).toBe(1);
+describe('Login Form', () => {
+    let wrapper;
+    
+
+
+    it('check email input', () => {
+        wrapper = shallow(<LoginForm />)
+        wrapper.find('input[type="email"]').simulate('change', { target: { name: 'inputEmail', value: 'login@form.test' } });
+        expect(wrapper.state('inputEmail')).toEqual('login@form.test');
+    })
+    it('password check', () => {
+        wrapper = shallow(<LoginForm />);
+        wrapper.find('input[type="password"]').simulate('change', { target: { name: 'inputPassword', value: 'password123' } });
+        expect(wrapper.state('inputPassword')).toEqual('password123');
+    })
+    it('login check submit', () => {
+        const submit = () => jest.fn();
+        wrapper = shallow(<LoginForm onSubmit={submit}/>);
+        wrapper.find('input[type="email"]').simulate('change', { target: { name: 'inputEmail', value: 'login@form.test' } });
+        wrapper.find('input[type="password"]').simulate('change', { target: { name: 'inputPassword', value: 'password123' } });
+        wrapper.find('button').simulate('click');
+        expect(submit).toHaveBeenCalledTimes(1);
+    })
 })
+
