e97a5275427dcb41eda38a8b355dddcf03aeadba
CJ <devcj.in@gmail.com>
2019-10-22 11:58:34 +0530

    login control test with router checks working

--- src/Components/LoginControl/LoginControl.test.js
+++ src/Components/LoginControl/LoginControl.test.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import LoginForm from '.././login/login';
-import RegisterForm from '.././register/register';
-import AddPost from '../addPost/addPost';
-import { Switch, Route, NavLink } from 'react-router-dom';
 import { MemoryRouter } from 'react-router-dom';
-import createBrowserHistory from '../../history';
 import { shallow, mount } from 'enzyme';
 import "../../setupTest"
 import { testStore } from '../../Utils';
 import LoginControl from './LoginControl';
-
+import { Provider } from "react-redux";
 
 describe('LoginControl', () => {
     it('check AddPost mounts when logged In', () => {
@@ -28,22 +23,32 @@ describe('LoginControl', () => {
         //console.log(wrapper.find((AddPost)))
         expect(wrapper.find('Connect(AddPost)')).toHaveLength(1);
     })
-    it('check link nav shows when logged Out', () => {
+    it('check link nav shows when logged out and renders login when clicked', () => {
         let wrapper;
         const setUp = (initialState = {}) => {
             const store = testStore(initialState);
-            const wrapper = shallow(<MemoryRouter><LoginControl store={store} /></MemoryRouter>).dive().dive().dive().dive().dive()
+            const wrapper = mount(<MemoryRouter initialEntries={['/']}><Provider store={store}><LoginControl /></Provider></MemoryRouter>)
             return wrapper;
         };
-
         const initialState = {
             isLoggedIn: false
         }
         wrapper = setUp(initialState);
-
-        console.log(wrapper.debug())
-        //console.log(wrapper.find((AddPost)))
-        expect(wrapper.find('Connect(AddPost)')).toHaveLength(1);
-        expect(wrapper.find(NavLink))
+        wrapper.find('a').at(0).simulate('click', { button: 0 })
+        expect(wrapper.find('button').at(0).text()).toBe('Login')     
+    })
+    it('check link nav shows when logged out and renders register when clicked', () => {
+        let wrapper;
+        const setUp = (initialState = {}) => {
+            const store = testStore(initialState);
+            const wrapper = mount(<MemoryRouter initialEntries={['/']}><Provider store={store}><LoginControl /></Provider></MemoryRouter>)
+            return wrapper;
+        };
+        const initialState = {
+            isLoggedIn: false
+        }
+        wrapper = setUp(initialState);
+        wrapper.find('a').at(2).simulate('click', { button: 0 })
+        expect(wrapper.find('button').at(0).text()).toBe('Register')
     })
 })
